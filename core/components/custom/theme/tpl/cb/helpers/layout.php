6e2c87a0d3602fe9ed57a2f49d940126b638117b
Uros Likar <Uros.Likar@tend.si>
2021-09-29 15:33:05 +0200

    unfinished commit

--- core/components/custom/theme/tpl/cb/helpers/layout.php
+++ core/components/custom/theme/tpl/cb/helpers/layout.php
@@ -6,10 +6,44 @@ $cssCol = [];
 
 if($justifyContent) array_push($cssMain,$justifyContent);
 
-array_push($cssSub,'col-'.$colWidth);
+switch($layout){
 
+	case 1:
+		switch($layout){
+			default: array_push($cssCol,'col-md-'.$colWidth); break;
+		}		
+	break;
+	
+	case 2:
+		switch($layout){
+			default: array_push($cssCol,'col-md-'.$colWidth); break;
+		}
+	break;
 
+	case 3:
+		switch($layout){
+			default: array_push($cssCol,'col-md-'.$colWidth); break;
+		}
+	break;
 
+	case 4:
+		switch($layout){
+			default:
+				array_push($cssCol,'col-md-6');
+				array_push($cssCol,'col-lg-'.$colWidth);
+			break;
+		}
+	break;
 
-$modx->setPlaceholder("css.main",implode(" ",$cssMain));
-$modx->setPlaceholder("css.col",implode(" ",$cssCol));
+	default:
+		array_push($cssCol,'col-md-'.$colWidth);
+	break;
+}
+
+array_push($cssCol,'border');
+
+//$modx->setPlaceholder("layout".$layout.".start",'<div class="'.implode(" ",$cssMain).'">');
+//$modx->setPlaceholder("layout".$layout.".end",'</div>');
+
+$modx->setPlaceholder("css".$layout.".main",implode(" ",$cssMain));
+$modx->setPlaceholder("css".$layout.".col",implode(" ",$cssCol));
