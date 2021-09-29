1047397f6f86a6018601c84646258bb270325b7c
Uroš Likar <uros.likar@gmail.com>
2021-09-29 09:21:01 +0200

    unfinished from home

--- core/components/custom/theme/tpl/cb/helpers/layout.php
+++ core/components/custom/theme/tpl/cb/helpers/layout.php
@@ -1,4 +1,15 @@
 <?php 
-
 /* Used in content blocks layouts to generate tags, classes, ect... */
 
+$cssMain = ['row'];
+$cssCol = [];
+
+if($justifyContent) array_push($cssMain,$justifyContent);
+
+array_push($cssSub,'col-'.$colWidth);
+
+
+
+
+$modx->setPlaceholder("css.main",implode(" ",$cssMain));
+$modx->setPlaceholder("css.col",implode(" ",$cssCol));
